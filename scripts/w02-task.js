ffc4381b5ac31732a3720ecb063b5fe0e80ee5c6
MYST-Drakonoid <apet2554@gmail.com>
2023-09-29 11:57:08 -0600

    git commit -m added w3 material

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -39,3 +39,7 @@ foodElement.innerHTML += `<br>${FavFoods}`;
 FavFoods.pop();
 
 foodElement.innerHTML += `<br>${FavFoods}`;
+
+
+let scores = [56, 78, 95, 77, 65, 86];
+let target = scores.indexOf(87);
